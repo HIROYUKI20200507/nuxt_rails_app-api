4909535e12f8911ab30a27f87b910a0f7c90f2da
ak-akimoto-peraichi <hi.akimoto@peraichi.co.jp>
2022-10-10 11:06:20 +0900

    update: test set up

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,5 +18,9 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def active_user
+    User.find_by(activated: true)
+  end
+
   # Add more helper methods to be used by all tests here...
 end
